2d4a4767d5b913c1b056dbb4dd87c5f422401052
Dante <dante.a.sanchez@gmail.com>
2013-04-02 00:40:11 -0300

    Update plotterwidget.h
    Replaced QFrame with QCustomPlot

--- plotter/plotterwidget.h
+++ plotter/plotterwidget.h
@@ -14,16 +14,17 @@
 #include <connection/connectionwidget.h>
 #include <parser/parserwidget.h>
 #include <parser/parserengine.h>
+#include <plotter/qcustomplot.h>
 #include <livelistwidget/livelistwidget.h>
 
 class PlotterWidget : public QWidget
 {
     Q_OBJECT
-
+    
 public:
     PlotterWidget(QWidget *parent = 0);
     ~PlotterWidget();
-
+    
 public slots:
     void updateConnections(QStringList);
     void updateParsers(QStringList*);
@@ -33,33 +34,35 @@ signals:
     void plotterConnectionRequest(QString);
     void plotterParserRequest(QString);
 private:
-
-//    QLabel *widgetNameLabel;
-//    QPushButton *newProfileButton;
+    
+    //    QLabel *widgetNameLabel;
+    //    QPushButton *newProfileButton;
     QComboBox *connectionBox;
     QComboBox *parserBox;
     QTableWidget *tableWidget;
-    QFrame *plotFrame;
-
-//    QHBoxLayout *topLayout;
+    //    QFrame *plotFrame;
+    
+    //    QHBoxLayout *topLayout;
     QHBoxLayout *dataSourceLayout;
     QHBoxLayout *contentLayout;
     QVBoxLayout *mainLayout;
-
+    
     ConnectionWidget *connectionWidget;
     ParserWidget *parserWidget;
     ParserEngine *parserEngine;
-
+    
+    QCustomPlot *customPlot;
+    
     quint8 calcRowCount();
-
+    
 private slots:
-     void changeConnection(QString);
-     void detachConnection(void);
-     void changeParser(QString);
-     void detachParser(void);
-     void populateParserTable();
-     void parsedDataReady(QList<RepeatedVector> parsedData);
-//     void variableListChanged();
+    void changeConnection(QString);
+    void detachConnection(void);
+    void changeParser(QString);
+    void detachParser(void);
+    void populateParserTable();
+    void parsedDataReady(QList<RepeatedVector> parsedData);
+    //     void variableListChanged();
 };
 
 #endif // CONNECTIONWIDGET_H
